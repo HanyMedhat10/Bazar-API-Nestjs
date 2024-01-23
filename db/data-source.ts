d3ae6d90d59847b59104f0bb7837e5a0d32b7fae
Hany Medhat <hany.medhat24@gmail.com>
2024-01-23 22:12:48 +0200

    database connection &env setup

---
+++ db/data-source.ts
@@ -0,0 +1,19 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+// import {dotenv} from 'dotenv';
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'pass123',
+  database: 'bazarapi',
+  //   autoLoadEntities: true,
+  entities: [],
+  migrations: [],
+  logging: false,
+  synchronize: false,
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT as any,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      // entities: [],
+      migrations: [],
+      logging: false,
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
