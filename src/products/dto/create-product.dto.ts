9598f9635d71fe0f3852b02596a1bcef3ad5a213
Hany Medhat <hany.medhat24@gmail.com>
2024-01-28 01:59:02 +0200

    Create product dto & check validation

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,31 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsPositive,
+  IsString,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+  @IsNotEmpty()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @IsPositive()
+  price: number;
+  @IsNotEmpty()
+  @IsNumber({ maxDecimalPlaces: 2 })
+  @Min(0)
+  stock: number;
+  @IsNotEmpty()
+  @IsArray()
+  images: string[];
+  @IsNotEmpty()
+  @IsNumber()
+  category: number;
+}
